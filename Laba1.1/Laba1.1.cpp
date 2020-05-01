36d7a5817ad8754f2525b06db1361936bdaf9b1c
Асафьев Александр <asafjew2015@yandex.ru>
2020-05-01 13:51:53 +0300

    Добавьте файлы проекта.

---
+++ Laba1.1/Laba1.1.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include "Windows.h"
+#include <cmath>
+
+using namespace std;
+
+double f1(double m) {
+	double v;
+	if (m < 0)
+		v = sin(m) * sin(m) + cos(m) * cos(m);
+	else
+		v = sin(m * m);
+	return v;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double x, y, b;
+	cout << "Введите первое значение ";
+	cin >> x;
+	cout << "Введите количество ";
+	cin >> y;
+	cout << "Введите шаг ";
+	cin >> b;
+
+	for (int i = 1; i <= y; i++, x += b) {
+		cout << "Результат " << f1(x) << " к значению " << x;
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
